b14bbef42cfa49e6df06f15b63d3f7833c1f0d54
Aigars Bedeicis <aigars.bedeicis@edu.rtu.lv>
2018-03-04 18:09:31 +0200

    Add option to show currencies as separate entries Instead of having mouseover currency entry every time you can just enable this and have separate entry that shows below currency

--- Exlist/Modules/Currency.lua
+++ Exlist/Modules/Currency.lua
@@ -7,6 +7,14 @@ local GetItemInfo = GetItemInfo
 local math, table = math, table
 local WrapTextInColorCode = WrapTextInColorCode
 local Exlist = Exlist
+local config_defaults = {
+  icon = "",
+  name = "Name",
+  type = "currency",
+  enabled = false,
+  showSeparate = false
+}
+
 local function AddRefreshOptions() end
 local function Updater(event)
   local t = {}
@@ -21,21 +29,31 @@ local function Updater(event)
   local cur = Exlist.ConfigDB.settings.currencies
 
   -- update all currencies
+  -- Check Setting Table
+  for name, t in pairs(cur) do
+    for i,v in pairs(config_defaults) do
+      if t[i] == nil then
+        t[i] = v
+      end
+    end
+  end 
+
   for i=1, GetCurrencyListSize() do
     local name, isHeader, _, _, _, count, icon = GetCurrencyListInfo(i)
     if cur[name] then
       currencyAmount[name] = count
     elseif not isHeader then
-      cur[name] = {icon = icon,name = name,type = "currency",enabled = false}
+      cur[name] = {icon = icon,name = name,type = "currency",enabled = false,showSeparate = false}
       currencyAmount[name] = count
     end 
   end
+
   for name,v in pairs(cur) do
     if v.type == "item" and v.enabled then
       local amount = GetItemCount(v.name,true)
-      table.insert(t.currency,{name=name,amount = amount, texture=v.icon})
+      table.insert(t.currency,{name=name,amount = amount, texture=v.icon,showSeparate = v.showSeparate})
     elseif v.enabled then
-      table.insert(t.currency,{name = name,amount = currencyAmount[name], texture=v.icon})
+      table.insert(t.currency,{name = name,amount = currencyAmount[name], texture=v.icon,showSeparate = v.showSeparate})
     end
   end
   Exlist.UpdateChar(key,t)
@@ -54,33 +72,81 @@ local function AddRefreshOptions()
             width = "full",
             name = "Enable/Disable Currencies you want to see"
         },
+        spacer0 = {
+          type = "description",
+          order = 1.05,
+          width = "full",
+          name = ""
+        },
+        label1 = {
+          type = "description",
+          order = 1.1,
+          fontSize = "medium",
+          width = "normal",
+          name = WrapTextInColorCode("Name","ffffd200")
+        },
+        label2 = {
+          type = "description",
+          order = 1.2,
+          fontSize = "medium",
+          width = "half",
+          name = WrapTextInColorCode("Enable","ffffd200")
+        },
+        label3 = {
+          type = "description",
+          order = 1.3,
+          fontSize = "medium",
+          width = "half",
+          name = WrapTextInColorCode("Show Separate","ffffd200")
+        },
         spacer1 = {
           type = "description",
-          order = 998,
-          width = "full",
-          name = "\n\n"
+          order = 1.4,
+          width = "normal",
+          name = ""
         },
         spacer2 = {
           type = "description",
-          order = 999   ,
+          order = 998,
           width = "full",
-          name = "\n\n"
-        }
+          name = "\n"
+        },
 
     }
   }
   -- update currencies
   Updater()
-
   local n = 1
   for name,t in pairs(cur) do
     n = n + 1  
-    options.args[name] = {
-        type = "toggle",
+    options.args[name..'desc'] = {
+        type = "description",
         order = n,
-        name = string.format("|T%s:20|t %s",t.icon,name),
-        get = function() return t.enabled end,
-        set = function(self,v) t.enabled = v  AddRefreshOptions() end
+        name = string.format("|T%s:15|t %s",t.icon,name),
+        width = "normal"
+    }
+    options.args[name..'enable'] = {
+      type = "toggle",
+      order = n+.1,
+      name = "  ",
+      width = "half",
+      get = function() return t.enabled end,
+      set = function(self,v) t.enabled = v  AddRefreshOptions() end
+    }
+    options.args[name..'showSeparate'] = {
+      type = "toggle",
+      order = n+.2,
+      width = "half",
+      name = "  ",
+      disabled = function() return not t.enabled end,
+      get = function() return t.showSeparate end,
+      set = function(self,v) t.showSeparate = v  AddRefreshOptions() end
+    }
+    options.args[name..'spacer'] = {
+      type = "description",
+      order = n+.3,
+      width = "normal",
+      name = "",
     }
   end
   options.args["itemInput"] ={
@@ -117,17 +183,31 @@ local function Linegenerator(tooltip,data,character)
     titleName = "Currency",
     data = data.money.gold .. "|cFFd8b21ag|r " .. data.money.silver .. "|cFFadadads|r " .. data.money.coppers .. "|cFF995813c|r",
   }
+  local extraInfos = {}
 
   local currency = data.currency
   if currency then
     local sideTooltip = {body = {},title= WrapTextInColorCode("Currency","ffffd200")}
+    local cur = Exlist.ConfigDB.settings.currencies
     for i=1,#currency do
+      if currency[i].showSeparate or cur[currency[i].name].showSeparate then
+        table.insert(extraInfos,{
+          character = character,
+          moduleName = key .. currency[i].name,
+          priority = prio+i/10,
+          titleName = "|T".. (currency[i].texture or "") ..":0|t" .. (currency[i].name or ""),
+          data = currency[i].amount,
+        })
+      end
       table.insert(sideTooltip.body,{"|T".. (currency[i].texture or "") ..":0|t" .. (currency[i].name or ""), currency[i].maxed and WrapTextInColorCode(currency[i].amount, "FFFF0000") or currency[i].amount})
     end
     table.insert(sideTooltip.body,"|cfff2b202To add additional items/currency check out config!")
     info.OnEnter = Exlist.CreateSideTooltip()
     info.OnEnterData = sideTooltip
     info.OnLeave = Exlist.DisposeSideTooltip()
+  end
+  for i,t in ipairs(extraInfos) do
+    Exlist.AddData(t)
   end
   Exlist.AddData(info)
 end
