da48696aa20e61921d6e62b59aed585373a20055
Aigars Bedeicis <aigars.bedeicis@edu.rtu.lv>
2018-04-24 11:25:06 +0300

    Sort quests in config by putting default quests on top

--- Exlist/Modules/Quests.lua
+++ Exlist/Modules/Quests.lua
@@ -4,6 +4,29 @@ local Exlist = Exlist
 local colors = Exlist.Colors
 local strings = Exlist.Strings
 
+local function spairs(t, order)
+  -- collect the keys
+  local keys = {}
+  for k in pairs(t) do keys[#keys + 1] = k end
+
+  -- if order function given, sort by it by passing the table and keys a, b,
+  -- otherwise just sort the keys
+  if order then
+    table.sort(keys, function(a, b) return order(t, a, b) end)
+  else
+    table.sort(keys)
+  end
+
+  -- return the iterator function
+  local i = 0
+  return function()
+    i = i + 1
+    if keys[i] then
+      return keys[i], t[keys[i]]
+    end
+  end
+end
+
 local checkFunctions = {}
 local questTypes = {
   ["daily"] = "Daily",
@@ -354,7 +377,9 @@ local function SetupQuestConfig(refresh)
     }
   }
   local n = 2
-  for questId,info in pairs(trackedQuests) do
+  for questId,info in spairs(trackedQuests, function(t,a,b) 
+    return t[a].default and not t[b].default
+  end) do
     local o = options.args
     o[questId.."enabled"] = {
         order = n,
