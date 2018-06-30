1bda0bed168a0ddb20c13ddd43ee7e8ec63473ec
Aigars Bedeicis <aigars.bedeicis@edu.rtu.lv>
2018-06-30 21:45:30 +0300

    Add available Azerite trait detection

--- Exlist/Modules/Azerite.lua
+++ Exlist/Modules/Azerite.lua
@@ -2,9 +2,41 @@ local key = "azerite"
 local prio = 30
 local Exlist = Exlist
 local L = Exlist.L
-local C_AzeriteItem = C_AzeriteItem
+local C_AzeriteItem,C_AzeriteEmpoweredItem = C_AzeriteItem,C_AzeriteEmpoweredItem
+local Item = Item
+local format = string.format
+local tinsert = tinsert
+local next, ipairs, pairs = next, ipairs, pairs
+local WrapTextInColorCode = WrapTextInColorCode
 local colors = Exlist.Colors
 --local strings = Exlist.Strings
+
+local function GetAvailableTraits(powerLevel)
+	local t = {}
+	for _,slot in next, { 1, 3, 5 } do
+		local item = Item:CreateFromEquipmentSlot(slot)
+		if not item:IsItemEmpty() then
+			local itemLoc = item:GetItemLocation()
+			if C_AzeriteEmpoweredItem.IsAzeriteEmpoweredItem(itemLoc) and C_AzeriteEmpoweredItem.HasAnyUnselectedPowers(itemLoc) then
+				local tiers = C_AzeriteEmpoweredItem.GetAllTierInfo(itemLoc)
+				t[slot] = 0
+				for _,tierInfo in ipairs(tiers) do
+					if tierInfo.unlockLevel <= powerLevel then
+						local empty = true
+						for _,powerId in ipairs(tierInfo.azeritePowerIDs) do
+							if C_AzeriteEmpoweredItem.IsPowerSelected(itemLoc,powerId) then
+								empty = false
+								break
+							end
+						end
+						t[slot] = empty and t[slot] + 1 or t[slot]
+					end
+				end
+			end
+		end
+	end
+	return t
+end
 
 local function GetAzeriteInfo()
   if not C_AzeriteItem.HasActiveAzeriteItem() then return end
@@ -15,6 +47,7 @@ local function GetAzeriteInfo()
     xp = xp,
     maxXp = maxXp,
     powerLevel = powerLevel,
+    traitsAvailable = GetAvailableTraits(powerLevel),
   }
   return t
 end
@@ -23,6 +56,12 @@ local function Updater(event)
   Exlist.UpdateChar(key,GetAzeriteInfo())
 end
 
+local slotNames = {
+	[1] = L["Head"],
+	[3] = L["Shoulders"],
+	[5] = L["Chest"],
+}
+
 local function Linegenerator(tooltip,data,character)
   if not data then return end
   local info = {
@@ -30,17 +69,26 @@ local function Linegenerator(tooltip,data,character)
     priority = prio,
     moduleName = key,
     titleName = L["Azerite Power"],
-    data = string.format("|c%s%s:|r %i",colors.faded,L["Level"],data.powerLevel),
+    data = format("|c%s%s:|r %i",colors.faded,L["Level"],data.powerLevel),
     OnEnter = Exlist.CreateSideTooltip(),
     OnEnterData = {
       title = WrapTextInColorCode(L["Azerite Power"], colors.sideTooltipTitle),
       body = {
-        { L["Progress"], string.format("%.1f%% (%i/%i)",(data.xp/data.maxXp)*100,data.xp,data.maxXp)},
+        { L["Progress"], format("%.1f%% (%i/%i)",(data.xp/data.maxXp)*100,data.xp,data.maxXp)},
         { L["Level"],data.powerLevel },
       }
     },
     OnLeave = Exlist.DisposeSideTooltip(),
   }
+  local shorten = Exlist.ConfigDB.settings.shortenInfo
+  local total = 0
+  for slot,availableTraits in pairs(data.traitsAvailable) do
+  	tinsert(info.OnEnterData.body,{format("%s: %s",slotNames[slot],WrapTextInColorCode(format("%i |4 %s: %s; %s",availableTraits,L["trait"],L["traits"],L["available"]), colors.available))})
+  	total = total + availableTraits
+  end
+  if total > 0 then 
+  	info.data = info.data .. WrapTextInColorCode(format(" %i %s",total,shorten and L["avail"] or L["available"]), colors.available)
+  end
   Exlist.AddData(info)
 end
 
@@ -77,7 +125,7 @@ local data = {
   linegenerator = Linegenerator,
   priority = prio,
   updater = Updater,
-  event = {"AZERITE_ITEM_EXPERIENCE_CHANGED","AZERITE_ITEM_POWER_LEVEL_CHANGED"},
+  event = {"PLAYER_ENTERING_WORLD","AZERITE_ITEM_EXPERIENCE_CHANGED","AZERITE_ITEM_POWER_LEVEL_CHANGED"},
   weeklyReset = false,
   dailyReset = false,
   description = L["<TMP> Tracks Azerite Item's current level and progress"],
