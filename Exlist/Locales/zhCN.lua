8168768b98e9e5479f1ee331d1e5f2e6a135e837
Aigars Bedeicis <aigars.bedeicis@edu.rtu.lv>
2018-10-26 22:27:11 +0300

    Update zhCN

--- Exlist/Locales/zhCN.lua
+++ Exlist/Locales/zhCN.lua
@@ -2,5 +2,6 @@ if not (GetLocale() == "zhCN") then
   return
 end
 local L = Exlist.L
+-- Thanks alot CrisReus (on Github) for initial translation
 
 --@localization(locale="zhCN", format="lua_additive_table", namespace="Exlist", handle-subnamespaces="none")@
