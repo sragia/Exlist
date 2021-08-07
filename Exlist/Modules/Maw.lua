787a54c79e255ee14240c73e3a5f86fbf7bf2f07
Aigars Bedeicis <a.bedeicis@gmail.com>
2021-08-07 14:22:52 +0300

    Remove Eye of the Jailer functionality

--- Exlist/Modules/Maw.lua
+++ Exlist/Modules/Maw.lua
@@ -3,32 +3,6 @@ local prio = 15
 local Exlist = Exlist
 local L = Exlist.L
 local colors = Exlist.Colors
---local strings = Exlist.Strings
-
-local MAW_WIDGETS = {
-  [2359] = 2873,
-  [2360] = 2874,
-  [2361] = 2875,
-  [2362] = 2876,
-  [2363] = 2877
-}
-
-local EOJ_STAGES = {
-  [2359] = 0,
-  [2360] = 1,
-  [2361] = 2,
-  [2362] = 3,
-  [2363] = 4
-}
-
-local STAGE_COLORS = {
-  [0] = "ffffffff",
-  [1] = "fff5ff82",
-  [2] = "fff5dd42",
-  [3] = "fff5aa42",
-  [4] = "fff56642",
-  [5] = "ffff0000"
-}
 
 local TORGHAST_WIDGETS = {
   {name = 2925, level = 2930}, -- Fracture Chambers
@@ -38,41 +12,6 @@ local TORGHAST_WIDGETS = {
   {name = 2928, level = 2938}, -- Mort'regar
   {name = 2929, level = 2940} -- The Upper Reaches
 }
-
-local function GetStageString(stage, stageProgress)
-  if (stageProgress >= 100) then
-    stage = stage + 1
-  end
-  return string.format("|c%s%s %i|r", STAGE_COLORS[stage or 0], L["Tier"], stage)
-end
-
-local function GetEyeOfTheJailerProgress(widgetId)
-  local info = C_UIWidgetManager.GetStatusBarWidgetVisualizationInfo(widgetId)
-  local stage = EOJ_STAGES[widgetId] or 0
-  if (info.barValue == 0) then
-    -- Returns as 0 when returning to Oribos
-    -- Cant catch it by zoneId as we are still in maw theoretically
-    return false
-  end
-  local stageProgress = info.barValue - info.barMin
-  return {
-    stage = stage or 0,
-    stageProgress = stageProgress > 0 and stageProgress or 0,
-    stageMax = info.barMax - info.barMin,
-    tooltip = info.tooltip
-  }
-end
-
-local function FindEyeOfTheJailerWidget()
-  for widgetId, shownWidgetId in pairs(MAW_WIDGETS) do
-    local info = C_UIWidgetManager.GetTextureWithAnimationVisualizationInfo(shownWidgetId)
-
-    if (info and info.shownState == 1) then
-      return GetEyeOfTheJailerProgress(widgetId)
-    end
-  end
-  return false
-end
 
 local function GetTorghastProgress()
   local data = {}
@@ -104,22 +43,6 @@ local function Updater(event, widgetInfo)
   if (UnitLevel("player") < Exlist.constants.MAX_CHARACTER_LEVEL) then
     return
   end
-  if (event == "UPDATE_UI_WIDGET") then
-    local widgetId = widgetInfo and widgetInfo.widgetID
-    if (widgetInfo and MAW_WIDGETS[widgetId]) then
-      local widget = MAW_WIDGETS[widgetId]
-      local widgetInfo = C_UIWidgetManager.GetTextureWithAnimationVisualizationInfo(widget)
-      if (widgetInfo and widgetInfo.shownState == 1) then
-        data.eoj = GetEyeOfTheJailerProgress(widgetId) or data.eoj
-      end
-    end
-  else
-    local eojData = FindEyeOfTheJailerWidget()
-
-    if (eojData) then
-      data.eoj = eojData
-    end
-  end
   data.torghast = GetTorghastProgress() or data.torghast
   Exlist.UpdateChar(key, data)
 end
@@ -127,38 +50,6 @@ end
 local function Linegenerator(tooltip, data, character)
   if (not data) then
     return
-  end
-  -- Maw - Eye of the Jailer
-  if (data.eoj) then
-    local stageProgress = (data.eoj.stageProgress / data.eoj.stageMax) * 100
-    local info = {
-      character = character,
-      priority = prio,
-      moduleName = key,
-      titleName = L["Eye of the Jailer"],
-      data = string.format(
-        "%s %.1f%%",
-        GetStageString(data.eoj.stage, stageProgress),
-        stageProgress >= 100 and 0 or stageProgress
-      )
-      -- colOff = 0,
-      -- dontResize = false,
-      -- pulseAnim = false,
-      -- OnClick = function() end,
-      -- OnClickData = {},
-    }
-
-    if (data.eoj.tooltip) then
-      info.OnEnter = Exlist.CreateSideTooltip()
-      info.OnEnterData = {
-        body = {
-          data.eoj.tooltip
-        },
-        title = GetStageString(data.eoj.stage, stageProgress)
-      }
-      info.OnLeave = Exlist.DisposeSideTooltip()
-    end
-    Exlist.AddData(info)
   end
 
   -- Torghast
@@ -198,10 +89,7 @@ local function init()
   -- code that will run before any other function
 end
 ]]
-local function ResetHandler(resetType)
-  -- code that will be run at reset for this module
-  -- instead of just wiping all data that is keyed
-  -- by this module key
+local function ResetHandler(resetType)\
   local realms = Exlist.GetRealmNames()
   for _, realm in ipairs(realms) do
     local characters = Exlist.GetRealmCharacters(realm)
@@ -209,14 +97,6 @@ local function ResetHandler(resetType)
       local data = Exlist.GetCharacterTableKey(realm, character, key)
       local essentials = Exlist.GetCharacterEssentials(realm, character)
       if (essentials.level == 60) then
-        -- Reset Eye of the Jailer debuff progress
-        if (resetType == "daily") then
-          data.eoj = {
-            stage = 0,
-            stageProgress = 0,
-            stageMax = 1000
-          }
-        end
         if (resetType == "weekly") then
           data.torghast = {}
         end
