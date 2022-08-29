c7bbec077e74fa871f6682385ed9a8b3f94fa52d
Aigars Bedeicis <a.bedeicis@gmail.com>
2022-08-30 00:40:32 +0300

    Fix whitespace

--- Exlist/Modules/Callings.lua
+++ Exlist/Modules/Callings.lua
@@ -63,7 +63,7 @@ local function Linegenerator(tooltip, data, character)
    )
    for _, calling in ipairs(data) do
       if (calling.endTime and time() <= calling.endTime) then
-         info.data = settings.shortenInfo and GetClockIcon(calling.endTime) or 
+         info.data = settings.shortenInfo and GetClockIcon(calling.endTime) or
             string.format("|T%s:45:45:::256:256:58:198:51:197|t %s", calling.icon or "", GetClockIcon(calling.endTime))
          local sideTooltip = {
             body = {},
